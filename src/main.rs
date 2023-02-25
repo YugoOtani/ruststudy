36f5ee0d594a42f7b53c3b8e33e8eedcda13cd62
YugoOtani <otnhugo0125@gmail.com>
2023-02-25 15:04:57 +0900

    first_commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use ctrlc;
+use std::any::type_name;
+use std::io;
+use std::io::Write;
+use std::{thread, time};
+fn interpret_line(input: String) {
+    thread::sleep(time::Duration::from_secs(3));
+    if &input[..] == "exit" {
+        std::process::exit(0);
+    }
+    println!("{:?}", input);
+}
+fn main() {
+    for i in 1.. {
+        print!("ysh[{i}] 🐈 > ");
+        io::stdout().flush().unwrap();
+        let mut buf = String::new();
+        match io::stdin().read_line(&mut buf) {
+            Ok(_) => interpret_line(buf.lines().collect()),
+            Err(error) => println!("error: {error}"),
+        }
+    }
+    //todo: ctrl-c and ctrl-d
+}
+
+fn tn<T>(_: T) {
+    println!("{:?}", type_name::<T>())
+}
