8af9fb183fca946a2cf06d02ec05403bfe3eb5a4
YugoOtani <otnhugo0125@gmail.com>
2023-02-27 06:56:49 +0900

    add lifetime parameter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use pom::parser::*;
-use pom::Parser;
 use std::any::type_name;
 use std::io::{stdin, stdout, Write};
 enum Ysh {
@@ -11,33 +10,28 @@ enum Ysh {
     YIn(Box<Ysh>, String),     // A < file
     YOut(Box<Ysh>, String),    // A > file
 }
-#[derive(Debug)]
-enum YshKw {
-    YSeq,
-    YAnd,
-    YOr,
-    YPipe,
-    YIn,
-    YOut,
-    YOpen,
-    YClose,
-    YToken(String),
-}
-
 fn main() {
-    loop {
-        let mut s = String::new();
-        print!("input words to parse > ");
-        stdout().flush().unwrap();
-        stdin().read_line(&mut s).unwrap();
-        let integer = one_of(b"0123456789").repeat(0..);
-        //let brace = sym(b'(') * integer - sym(b')');
-        tn(&integer);
-        println!("{:?} {:?}", s.as_bytes(), integer.parse(&s.as_bytes()));
-        stdout().flush().unwrap();
-    }
+    let mut s = String::new();
+    stdout().flush().unwrap();
+    stdin().read_line(&mut s).unwrap();
+    println!("{:?}", exp().parse(s.as_bytes()));
+}
+fn exp<'a>() -> Parser<'a, u8, Vec<u8>> {
+    let integer = one_of(b"0123456789").repeat(0..);
+    let space = sym(b' ').repeat(0..);
+    end().map(|_| vec![])
+        | (integer + space * call(exp)).map(|(mut v, mut e)| {
+            v.append(&mut e);
+            v
+        })
+}
+fn intp<'a>() -> Parser<'a, u8, Vec<u8>> {
+    one_of(b"0123456789").repeat(0..)
+}
+fn space<'a>() -> Parser<'a, u8, ()> {
+    one_of(b" \t\r\n").repeat(0..).discard()
 }
 
-fn tn<T>(_: T) {
+fn tn<T>(_: &T) {
     println!("{:?}", type_name::<T>())
 }
