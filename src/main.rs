4049cbbe0d5927d320e12920e5f183d11c0c4ef8
YugoOtani <otnhugo0125@gmail.com>
2023-02-27 17:58:34 +0900

    implement ycommand parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use pom::parser::is_a;
 use pom::parser::*;
 use std::any::type_name;
 use std::io::{stdin, stdout, Write};
+
+const RESERVED_CHAR: &[u8; 11] = b";&|<>() \n\t\r";
+#[derive(Debug)]
 enum Ysh {
     YCommand(String),
     YSeq(Box<Ysh>, Box<Ysh>),  // A; B
@@ -9,12 +13,17 @@ enum Ysh {
     YPipe(Box<Ysh>, Box<Ysh>), // A | B
     YIn(Box<Ysh>, String),     // A < file
     YOut(Box<Ysh>, String),    // A > file
+    YSub(Box<Ysh>),            // (A)
 }
 fn main() {
     let mut s = String::new();
     stdout().flush().unwrap();
     stdin().read_line(&mut s).unwrap();
-    println!("{:?}", exp().parse(s.as_bytes()));
+    println!("{:?}", p_command().parse(s.as_bytes()));
+}
+fn p_command<'a>() -> Parser<'a, u8, Ysh> {
+    let com = space() * none_of(RESERVED_CHAR).repeat(1..) - space();
+    com.convert(|u8s| String::from_utf8(u8s)).map(Ysh::YCommand)
 }
 fn exp<'a>() -> Parser<'a, u8, Vec<u8>> {
     let integer = one_of(b"0123456789").repeat(0..);
@@ -24,9 +33,6 @@ fn exp<'a>() -> Parser<'a, u8, Vec<u8>> {
             v.append(&mut e);
             v
         })
-}
-fn intp<'a>() -> Parser<'a, u8, Vec<u8>> {
-    one_of(b"0123456789").repeat(0..)
 }
 fn space<'a>() -> Parser<'a, u8, ()> {
     one_of(b" \t\r\n").repeat(0..).discard()
