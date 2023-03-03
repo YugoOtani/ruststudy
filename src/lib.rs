dac2f604d06eba5946fe87f377950cc33d9fe032
YugoOtani <otnhugo0125@gmail.com>
2023-03-04 05:01:23 +0900

    use_myparser

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod myparser;
 pub mod yparser;
+
--- src/yparser.rs
+++ src/yparser.rs
@@ -1,7 +1,5 @@
-
 //todo:https://en.wikipedia.org/wiki/Parsing_expression_grammar
-const SPACE_CHARS: &[u8; 4] = b" \n\t\r";
-const RESERVED_CHARS: &[u8; 11] = b";&|<>() \n\t\r";
+const RESERVED_CHARS: &str = ";&|<>()";
 #[derive(Debug)]
 pub enum Ysh {
     Command(Command),
@@ -92,8 +90,14 @@ impl Command {
         }
     }
 }
+
 pub fn parse_ysh(s: String) -> Result<Ysh, String> {
-    p_a(s)
+    let (ysh, s) = p_a(s.lines().collect())?;
+    if s == format!("") {
+        Ok(ysh)
+    } else {
+        Err(format!("{s} remains untaken"))
+    }
 }
 // S := a
 // a := b;a  b&&a  b|a  b||a  b
@@ -101,31 +105,94 @@ pub fn parse_ysh(s: String) -> Result<Ysh, String> {
 // b := (a) | C
 
 // S := a
-// a := b;ax  b&&ax  b|ax  b||ax  bx
-// x := >STR;ax  >STR&&ax >STR|ax >STR||ax >STRx end
+// a := b;t  b&&t  b|t  b||t  bx
+// x := >STR;t  >STR&&t >STR|t >STR||t >STRx ////end
+// t := ax
 // b := (a) | C
 
-enum X{
-    Seq(Ysh),And(Ysh),Pipe(Ysh),Or(Ysh)
-}
-fn p_a(s: String) -> Result<Ysh, String> {
-    let (b, s) = p_b(s);
-    let s = del_space(s);
-    match look_n(&s,2) {
-        Some("&&") => p_a(del_n(s,2)).and_then(|c| p_x(c,))
-            let x=p_x(s);
-        ,
-        Some("||") => {
-            p_a(del_n(s,2));
+fn p_a(s: String) -> Result<(Ysh, String), String> {
+    let (b, s) = p_b(del_space(s))?;
+    match look_n(&s, 2) {
+        Some("&&") => {
+            let (t, s) = p_t(del_n(s, 2))?;
+            Ok((y_and(b, t), s))
         }
+        Some("||") => {
+            let (t, s) = p_t(del_n(s, 2))?;
+            Ok((y_or(b, t), s))
+        }
+        _ => match look_1(&s) {
+            Some(';') => {
+                let (t, s) = p_t(del_1(s))?;
+                Ok((y_seq(b, t), s))
+            }
+            Some('|') => {
+                let (t, s) = p_t(del_1(s))?;
+                Ok((y_pipe(b, t), s))
+            }
+            Some('<') | Some('>') | Some(')') => p_x(b, s),
+            Some(c) => Err(format!("unexpected '{c}' in p_a")),
+            None => Ok((b, format!(""))),
+        },
     }
-    todo!()
 }
-fn p_x(ysh:Ysh,s: String) -> Result<Ysh,String> {
-    todo!()
+fn p_x(ysh: Ysh, s: String) -> Result<(Ysh, String), String> {
+    let s = del_space(s);
+    let (r, s) = match look_1(&s) {
+        Some('>') => {
+            let (fname, s) = take_string(del_space(del_1(s)))?;
+            (y_out(ysh, fname), s)
+        }
+        Some('<') => {
+            let (fname, s) = take_string(del_space(del_1(s)))?;
+            (y_in(ysh, fname), s)
+        }
+        None | Some(')') => return Ok((ysh, s)),
+        Some(s) => return Err(format!("unexpected {s} in p_x")),
+    };
+    let s = del_space(s);
+    match look_n(&s, 2) {
+        Some("&&") => {
+            let (t, s) = p_t(del_n(s, 2))?;
+            Ok((y_and(r, t), s))
+        }
+        Some("||") => {
+            let (t, s) = p_t(del_n(s, 2))?;
+            Ok((y_or(r, t), s))
+        }
+        _ => match look_1(&s) {
+            Some('|') => {
+                let (t, s) = p_t(del_space(del_1(s)))?;
+                Ok((y_pipe(r, t), s))
+            }
+            Some(';') => {
+                let (t, s) = p_t(del_space(del_1(s)))?;
+                Ok((y_seq(r, t), s))
+            }
+            Some('<') | Some('>') => p_x(r, s),
+            None | Some(')') => Ok((r, s)),
+            Some(c) => Err(format!("unexpected {c} in p_x")),
+        },
+    }
 }
-fn p_b(s: String) -> (Ysh, String) {
-    todo!()
+fn p_b(s: String) -> Result<(Ysh, String), String> {
+    let s = del_space(s);
+    match look_1(&s) {
+        Some('(') => {
+            let (a, s) = p_a(del_1(s))?;
+            if let Some(')') = look_1(&s) {
+                Ok((y_sub(a), del_1(s)))
+            } else {
+                Err(format!("unexpected {:?} in p_b", look_1(&s)))
+            }
+        }
+        _ => take_com(s),
+    }
+}
+fn p_t(s: String) -> Result<(Ysh, String), String> {
+    let s = del_space(s);
+    let (a, s) = p_a(s)?;
+    p_x(a, s)
 }
 fn look_1(s: &String) -> Option<char> {
     s.chars().nth(0)
@@ -145,4 +212,59 @@ fn del_n(s: String, n: usize) -> String {
 }
 fn del_space(s: String) -> String {
     s.chars().skip_while(|c| c.is_whitespace()).collect()
+}
+fn take_string(s: String) -> Result<(String, String), String> {
+    let s = del_space(s);
+    let f = |c: char| (RESERVED_CHARS.contains(|c2| c2 == c) || c.is_whitespace());
+    match s.find(f) {
+        Some(0) => Err(format!("can't take string from {s}")),
+        Some(i) => {
+            let mut lft = s.to_string();
+            let ret = lft.drain(..i).collect();
+            Ok((ret, lft))
+        }
+        None => Ok((s, "".to_string())),
+    }
+}
+fn take_com(s: String) -> Result<(Ysh, String), String> {
+    let s = del_space(s);
+    let f = |c: char| RESERVED_CHARS.contains(|c2| c2 == c);
+    let splw = |s: String| s.split_whitespace().map(String::from).collect();
+    match s.find(f) {
+        Some(0) => Err(format!("can't take command from {s}")),
+        Some(i) => {
+            let mut lft = s.to_string();
+            let rcom = Command::new(splw(lft.drain(..i).collect()));
+            rcom.map(move |com| (Ysh::Command(com), lft))
+        }
+        None => Command::new(splw(s)).map(move |com| (Ysh::Command(com), "".to_string())),
+    }
+}
+
+fn y_seq(l: Ysh, r: Ysh) -> Ysh {
+    Ysh::Seq(Box::new(l), Box::new(r))
+}
+fn y_and(l: Ysh, r: Ysh) -> Ysh {
+    Ysh::And(Box::new(l), Box::new(r))
+}
+fn y_or(l: Ysh, r: Ysh) -> Ysh {
+    Ysh::Or(Box::new(l), Box::new(r))
+}
+fn y_pipe(l: Ysh, r: Ysh) -> Ysh {
+    Ysh::Pipe(Box::new(l), Box::new(r))
+}
+fn y_in(l: Ysh, s: String) -> Ysh {
+    Ysh::In(Box::new(l), s)
+}
+fn y_out(l: Ysh, s: String) -> Ysh {
+    Ysh::Out(Box::new(l), s)
+}
+fn y_sub(y: Ysh) -> Ysh {
+    Ysh::Sub(Box::new(y))
+}
+
+#[test]
+fn test_delspace() {
+    println!("{:?}", take_com("  ls>out".to_string()));
+    assert!(false);
 }
